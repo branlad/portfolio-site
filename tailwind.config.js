70baefe16fe167134228237971801e406a5ce72b
Brandon <bladner0717@gmail.com>
2024-07-31 22:06:29 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {
+      fontSize: {
+        '3_5xl': '50px',
+      },
+    },
+  },
+  plugins: [],
+}
